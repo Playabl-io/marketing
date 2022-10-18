e4226cc323fee239471ee1792989c848dff5022d
Jonathan Grim <jonjongrim@gmail.com>
2022-10-18 00:42:15 -0400

    add comparison and other sections

---
+++ src/components/GhostButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <base-button
+    class="bg-inherit hover:bg-gray-100 dark:hover:bg-gray-700 text-current dark:text-slate-100"
+    v-bind="$attrs"
+  >
+    <slot></slot>
+  </base-button>
+</template>
+<script setup lang="ts">
+import BaseButton from "./BaseButton.vue";
+</script>
